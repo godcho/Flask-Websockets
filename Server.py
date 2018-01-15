daeaa6a1b501bc9ab251b55a69214668c150a642
gdy <gdy@exemple.com>
2018-01-15 10:22:22 +0100

    Nouvelle version Server + chgt echelle index.html

--- Server.py
+++ Server.py
@@ -107,5 +107,4 @@ def value_changed(message):
 #########################################################################
     
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0')
-    #app.run()
+    socketio.run(app, host='0.0.0.0',debug=True)
